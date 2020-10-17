cb945a3d4b64a261a0fb8f322d8b8eb762b4f22d
Shahan Rahman <shahan@Shahans-MBP.fios-router.home>
2020-10-17 16:06:19 -0400

    changed number in index

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function sendText(message){
 
   let encoded = new URLSearchParams()
   encoded.append("To", RECIPIENT)
-  encoded.append("From", "+19388887573")
+  encoded.append("From", "+3479017580")
   encoded.append("Body", message)
 
   let token = btoa(ACCOUNT_SID + ":" + AUTH_TOKEN)
